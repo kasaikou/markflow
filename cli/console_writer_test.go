acef5a8af2fbf873ee07d3c36e871c664ad2ce7e
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2024-02-15 10:54:28 +0000

    add console_writer

---
+++ cli/console_writer_test.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"sync"
+	"testing"
+)
+
+func TestConsoleWriter(t *testing.T) {
+
+	wg := sync.WaitGroup{}
+	defer wg.Wait()
+
+	cw, _ := NewConsoleWriter(os.Stdout)
+	defer cw.Close()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		cw.Route()
+	}()
+
+	taskWg := sync.WaitGroup{}
+	defer taskWg.Wait()
+
+	for i := 0; i < 100; i++ {
+		taskWg.Add(1)
+		go func(ch chan<- ConsoleRecord, record ConsoleRecord) {
+			defer taskWg.Done()
+			for i := 0; i < 100; i++ {
+				record.Text = strings.Repeat("a", rand.Intn(200))
+				ch <- record
+			}
+
+		}(cw.chRecord, ConsoleRecord{
+			RecordMode: RecordModeLF,
+			Kind:       NewStringWidth("TEST"),
+			Label:      NewStringWidth(fmt.Sprintf("TEST_%d", i)),
+			LabelDecoration: Decoration{
+				Background: fmt.Sprintf("\033[3%dm", i%8),
+			},
+		})
+	}
+}
--- cli/decorate.go
+++ cli/decorate.go
@@ -26,90 +26,143 @@ func (d Decoration) Push(expr []byte) Decoration {
 		first = begin + idx[0]
 		last = begin + idx[1]
 		expr := unsafe.String(unsafe.SliceData(expr[first:last]), last-first)
-
-		switch {
-		case expr == "\033[0m":
-			d.Bold = ""
-			d.Italic = ""
-			d.Underline = ""
-			d.Display = ""
-			d.Foreground = ""
-			d.Background = ""
-
-		case expr == "\033[1m":
-			if strings.HasSuffix(d.Bold, "\033[2m") {
-				d.Bold = strings.TrimSuffix(d.Bold, "\033[2m")
-			} else {
-				d.Bold += expr
-			}
-
-		case expr == "\033[2m":
-			if strings.HasSuffix(d.Bold, "\033[1m") {
-				d.Bold = strings.TrimSuffix(d.Bold, "\033[1m")
-			} else {
-				d.Bold += expr
-			}
-
-		case expr == "\033[3m":
-			if strings.HasSuffix(d.Italic, "\033[23m") {
-				d.Italic = strings.TrimSuffix(d.Italic, "\033[23m")
-			} else {
-				d.Italic = expr
-			}
-
-		case expr == "\033[4m":
-			if strings.HasSuffix(d.Underline, "\033[24m") {
-				d.Underline = strings.TrimSuffix(d.Underline, "\033[24m")
-			} else {
-				d.Underline = expr
-			}
-
-		case expr == "\033[8m":
-			if strings.HasSuffix(d.Display, "\033[28m") {
-				d.Display = strings.TrimSuffix(d.Display, "\033[28m")
-			} else {
-				d.Display = expr
-			}
-
-		case expr == "\033[22m":
-			d.Bold = ""
-
-		case expr == "\033[23m":
-			if strings.HasSuffix(d.Italic, "\033[3m") {
-				d.Italic = strings.TrimSuffix(d.Italic, "\033[3m")
-			} else {
-				d.Italic = expr
-			}
-
-		case expr == "\033[24m":
-			if strings.HasSuffix(d.Underline, "\033[4m") {
-				d.Underline = strings.TrimSuffix(d.Underline, "\033[4m")
-			} else {
-				d.Underline = expr
-			}
-
-		case expr == "\033[28m":
-			if strings.HasSuffix(d.Display, "\033[8m") {
-				d.Display = strings.TrimSuffix(d.Display, "\033[8m")
-			} else {
-				d.Display = expr
-			}
-
-		case expr == "\033[39m":
-			d.Foreground = ""
-
-		case expr == "\033[49m":
-			d.Background = ""
-
-		case strings.HasPrefix(expr, "\033[3") || strings.HasPrefix(expr, "\033[9"):
-			d.Foreground = expr
-
-		case strings.HasPrefix(expr, "\033[4") || strings.HasPrefix(expr, "\033[10"):
-			d.Background = expr
-		}
+		d = d.update(expr)
 
 		begin = last
 	}
 
 	return d
+}
+
+func (d Decoration) PushString(expr string) Decoration {
+	begin := 0
+	first := 0
+	last := 0
+
+	for idx := decorateRegexp.FindStringIndex(expr[begin:]); idx != nil; idx = decorateRegexp.FindStringIndex(expr[begin:]) {
+		first = begin + idx[0]
+		last = begin + idx[1]
+		expr := expr[first:last]
+		d = d.update(expr)
+
+		begin = last
+	}
+
+	return d
+}
+
+func (d Decoration) update(expr string) Decoration {
+	switch {
+	case expr == "\033[0m":
+		d.Bold = ""
+		d.Italic = ""
+		d.Underline = ""
+		d.Display = ""
+		d.Foreground = ""
+		d.Background = ""
+
+	case expr == "\033[1m":
+		if strings.HasSuffix(d.Bold, "\033[2m") {
+			d.Bold = strings.TrimSuffix(d.Bold, "\033[2m")
+		} else {
+			d.Bold += expr
+		}
+
+	case expr == "\033[2m":
+		if strings.HasSuffix(d.Bold, "\033[1m") {
+			d.Bold = strings.TrimSuffix(d.Bold, "\033[1m")
+		} else {
+			d.Bold += expr
+		}
+
+	case expr == "\033[3m":
+		if strings.HasSuffix(d.Italic, "\033[23m") {
+			d.Italic = strings.TrimSuffix(d.Italic, "\033[23m")
+		} else {
+			d.Italic = expr
+		}
+
+	case expr == "\033[4m":
+		if strings.HasSuffix(d.Underline, "\033[24m") {
+			d.Underline = strings.TrimSuffix(d.Underline, "\033[24m")
+		} else {
+			d.Underline = expr
+		}
+
+	case expr == "\033[8m":
+		if strings.HasSuffix(d.Display, "\033[28m") {
+			d.Display = strings.TrimSuffix(d.Display, "\033[28m")
+		} else {
+			d.Display = expr
+		}
+
+	case expr == "\033[22m":
+		d.Bold = ""
+
+	case expr == "\033[23m":
+		if strings.HasSuffix(d.Italic, "\033[3m") {
+			d.Italic = strings.TrimSuffix(d.Italic, "\033[3m")
+		} else {
+			d.Italic = expr
+		}
+
+	case expr == "\033[24m":
+		if strings.HasSuffix(d.Underline, "\033[4m") {
+			d.Underline = strings.TrimSuffix(d.Underline, "\033[4m")
+		} else {
+			d.Underline = expr
+		}
+
+	case expr == "\033[28m":
+		if strings.HasSuffix(d.Display, "\033[8m") {
+			d.Display = strings.TrimSuffix(d.Display, "\033[8m")
+		} else {
+			d.Display = expr
+		}
+
+	case expr == "\033[39m":
+		d.Foreground = ""
+
+	case expr == "\033[49m":
+		d.Background = ""
+
+	case strings.HasPrefix(expr, "\033[3") || strings.HasPrefix(expr, "\033[9"):
+		d.Foreground = expr
+
+	case strings.HasPrefix(expr, "\033[4") || strings.HasPrefix(expr, "\033[10"):
+		d.Background = expr
+	}
+
+	return d
+}
+
+func (d *Decoration) AppendBytes(src []byte) []byte {
+
+	src = append(src, "\033[0m"...)
+
+	if len(d.Bold) > 0 {
+		src = append(src, d.Bold...)
+	}
+
+	if len(d.Italic) > 0 {
+		src = append(src, d.Italic...)
+	}
+
+	if len(d.Underline) > 0 {
+		src = append(src, d.Italic...)
+	}
+
+	if len(d.Display) > 0 {
+		src = append(src, d.Display...)
+	}
+
+	if len(d.Foreground) > 0 {
+		src = append(src, d.Foreground...)
+	}
+
+	if len(d.Background) > 0 {
+		src = append(src, d.Background...)
+	}
+
+	return src
 }
